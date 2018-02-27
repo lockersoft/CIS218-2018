db9b9ccd0e7dfce00b46be6e7194a06690a47f25
Dave Jones <dave@lockersoft.com>
2018-02-27 10:17:42 -0800

    added conversion math example code

--- app/src/main/java/com/example/davejones/myapplication/ConversionActivity.java
+++ app/src/main/java/com/example/davejones/myapplication/ConversionActivity.java
@@ -10,10 +10,14 @@ import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ConversionActivity extends BaseActivity {
 
   String resultStr = "";
   EditText edtConversionResult;
+  EditText edtResultFrom;
 
   ListView listView;
   String[] items = {
@@ -22,51 +26,76 @@ public class ConversionActivity extends BaseActivity {
       "Miles to Kilometers",
       "Kilometers to Miles",
       "Gallons to Cups",
-      "Cups to Gallons",
-      "Inches to Centimeters",
-      "Centimeters to Inches",
-      "Miles to Kilometers",
-      "Kilometers to Miles",
-      "Gallons to Cups",
-      "Cups to Gallons",
-      "Inches to Centimeters",
-      "Centimeters to Inches",
-      "Miles to Kilometers",
-      "Kilometers to Miles",
-      "Gallons to Cups",
       "Cups to Gallons"
   };
+
+
+//  id => [description, price ]
+  Double[] conversionRates = {
+      2.54,
+      0.393701,
+      1.60934,
+      0.6213727366
+
+  };
+
+  Map<String, Double> conversionMap = new HashMap<String, Double>();
 
   @Override
   protected void onCreate( Bundle savedInstanceState ) {
     super.onCreate( savedInstanceState );
     setContentView( R.layout.activity_conversion );
 
+
+//    conversionMap.put("Inches to Centimeters", 2.56);
+
     edtConversionResult = findViewById( R.id.edtResultTo );
+    edtResultFrom = findViewById( R.id.edtResultFrom );
 
     // Get The Intent data
     Intent intent = getIntent();
     resultStr = intent.getStringExtra( "RESULT" );
     Log.i( "CALC", "ResultStr: " + resultStr );
     edtConversionResult.setText( resultStr );
-    ToastIt( getString( R.string.runningOnCreate) );
+//    ToastIt( getString( R.string.runningOnCreate) );
 
-    ArrayAdapter adapter = new ArrayAdapter<String>( this, R.layout.activity_listview, items );  // TODO:  change layout to simple layout with only text view in it.
+    Double test = 0.5555555555E20;
+    edtResultFrom.setText( Double.toString( test ) );
+    Double test1 = Double.parseDouble( edtResultFrom.getText().toString() );
+
+    ToastIt( "Double Test: " + (test.equals( test1 ) ? "TRUE" : "FALSE" ));
+
+    ArrayAdapter adapter = new ArrayAdapter<String>( this, R.layout.activity_listview, items );
     listView = (ListView)findViewById( R.id.listView );
     listView.setAdapter( adapter );
 
-//    listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-//      @Override
-//      public void onItemClick(AdapterView<?> parent, View view,
-//                              int position, long id) {
-//        Toast.makeText(ConversionActivity.this, "You Clicked at " + items[position], Toast.LENGTH_SHORT).show();
-//        performConversion(  position );
-//      }
-//    });
+    listView.setOnItemClickListener( new AdapterView.OnItemClickListener(){
+
+      @Override
+      public void onItemClick( AdapterView<?>parent, View view, int position, long id){
+        performCalculation(position);
+      }
+    } );
   }
 
-//  private void performConversion( int position ) {
-//  }
+  private void performCalculation( int position ){
+    Double fromNumber,toNumber;
+    ToastIt( "You Clicked at: " + items[position] );
+    fromNumber = Double.parseDouble( edtResultFrom.getText().toString() );
+    toNumber = conversionRates[position] * fromNumber;
+    edtConversionResult.setText( Double.toString( toNumber));
+//
+//    if( position == 0 ) {   // Inches to centimeters
+//      fromNumber = Double.parseDouble( edtResultFrom.getText().toString() );
+//      centimeters = fromNumber * 2.54;
+//      edtConversionResult.setText( Double.toString( centimeters));
+//    }
+//    if( position == 1 ) {   // centimeters Inches to
+//      fromNumber = Double.parseDouble( edtResultFrom.getText().toString() );
+//      centimeters = fromNumber * 0.393701;
+//      edtConversionResult.setText( Double.toString( centimeters));
+//    }
+  }
 
     public void calcButtonOnClick( View v ){
     // Switch to the other activity - calc
